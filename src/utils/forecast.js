c137fdd350f5e3c2c106c364cf8d756fa6c423fe
Felix Curiel <felicuriel15@hotmail.com>
2022-03-25 23:54:40 -0500

    New meteorological information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,8 @@ const forecast = (latitude,longitude, callback)=>{
     }else if(response.body.error){
     callback('Unable to find location. Try another search.', undefined)
     }else{
-        callback(undefined, response.body.current.weather_descriptions[0]+'. It is currently '+ response.body.current.temperature + ' degrees out. It feels like ' +response.body.current.feelslike)
+        callback(undefined, response.body.current.weather_descriptions[0]+'. It is currently '+ response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike+ 'The humidity is '+ response.body.current.humidity + '%')
+    
     }
     })
     }
